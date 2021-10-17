c96cbb776036e3cda4e13811b07448a4bebf1fa1
seoyoungsoo <iulove37@naver.com>
2021-10-17 21:58:47 +0900

    feat: msgInput component add

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MsgInput from './MsgInput';
 import MsgItem from './MsgItem';
 
 const UserIds = ['roy', 'jay'];
 const getRandomUserId = () => UserIds[Math.round(Math.random())];
 
+const originalMsgs = Array(50)
+  .fill(0)
+  .map((_, i) => ({
+    id: i + 1,
+    userId: getRandomUserId(),
+    timestamp: 1234567890123 + i * 1000 * 60,
+    text: `${i + 1} mock text`,
+  }))
+  .reverse();
+
 const MsgList = () => {
-  const msgs = Array(50)
-    .fill(0)
-    .map((_, i) => ({
-      id: i + 1,
+  const [msgs, setMsgs] = useState(originalMsgs);
+
+  const onCreate = (text) => {
+    const newMsg = {
+      id: msgs.length + 1,
       userId: getRandomUserId(),
-      timestamp: 1234567890123 + i * 1000 * 60,
-      text: `${i + 1} mock text`,
-    }))
-    .reverse();
+      timestamp: Date.now(),
+      text: `${msgs.length + 1} ${text}`,
+    };
+    setMsgs((msgs) => [newMsg, ...msgs]);
+  };
 
   return (
-    <ul className='messages'>
-      {msgs.map((v) => (
-        <MsgItem key={v.id} {...v} />
-      ))}
-    </ul>
+    <>
+      <MsgInput mutate={onCreate} />
+      <ul className='messages'>
+        {msgs.map((v) => (
+          <MsgItem key={v.id} {...v} />
+        ))}
+      </ul>
+    </>
   );
 };
 
