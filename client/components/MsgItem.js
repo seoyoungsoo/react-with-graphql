72cc80268c8a0e343b9c052c76db47d30d4a76b3
seoyoungsoo <iulove37@naver.com>
2021-10-17 21:59:34 +0900

    chore: className change

--- client/components/MsgItem.js
+++ client/components/MsgItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MsgItem = ({ userId, timestamp, text }) => {
   return (
-    <ul className='messages_item'>
+    <ul className='messages__item'>
       <h3>
         {userId}{' '}
         <sub>
